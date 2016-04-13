be29005d8fb5f355f2649f1866e5996f56f2ec42
danglotb <bdanglot@gmail.com>
2016-04-13 14:34:36 +0200

    new results and new img

---
+++ src/main/java/quicksort/QuickSortCallableImpl.java
@@ -0,0 +1,20 @@
+package quicksort;
+
+import experiment.CallableImpl;
+
+import java.util.List;
+
+/**
+ * Created by spirals on 05/04/16.
+ */
+public class QuickSortCallableImpl extends CallableImpl<List<Integer>> {
+
+    public QuickSortCallableImpl(List<Integer> originalValue) {
+        super(originalValue);
+    }
+
+    @Override
+    public List<Integer> call() throws Exception {
+        return QuickSort.sort(super.originalValue);
+    }
+}
---
+++ src/main/java/quicksort/QuickSortOracleImpl.java
@@ -0,0 +1,66 @@
+package quicksort;
+
+import experiment.OracleImpl;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by spirals on 05/04/16.
+ */
+public class QuickSortOracleImpl extends OracleImpl<List<Integer>> {
+
+    @Override
+    protected List<Integer> generateOneTask() {
+        List<Integer> newTask = new ArrayList<>();
+        for (int i = 0 ; i < sizeOfEachTask ; i++)
+            newTask.add(randomForGenTask.nextInt());
+        return newTask;
+    }
+
+    public QuickSortOracleImpl() {
+        super();
+        super.path = "quicksort";
+    }
+
+    @Override
+    public String header() {
+        String header = numberOfTask + " arrays of " + sizeOfEachTask + " integers\n";
+        header += "Random integer generated with " + seedForGenTask + " as seed\n";
+        return header;
+    }
+
+    @Override
+    public boolean check(List<Integer> perturbedValue, int index) {
+        List<Integer> clone = new ArrayList<Integer>(scenario.get(index));
+
+        Iterator<Integer> it = perturbedValue.iterator();
+
+        int previousValue = it.next();
+
+        if (!clone.contains(previousValue))
+            return false;
+        else
+            clone.remove(clone.indexOf(previousValue));
+
+        while (it.hasNext()) {
+            int current = it.next();
+            if (current < previousValue)
+                return false;
+            else if (!clone.contains(current))
+                return false;
+            else
+                clone.remove(clone.indexOf(current));
+
+            previousValue = current;
+        }
+        return clone.isEmpty();
+    }
+
+    @Override
+    public List<Integer> get(int index) {
+        return new ArrayList(scenario.get(index));
+    }
+
+}
